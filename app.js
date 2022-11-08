c4996b4a0cdab5bfe33617f9a9fa6ecffae29c05
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-08 18:07:12 -0500

    feat: add static in express

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ const app = express()
 const server = app.listen(8080, () => console.log('Server Up'))
 
 app.use(express.json())
+
+app.use('/content', express.static('public'))
+
 app.use('/users', usersRouter)
 app.use('/pets', petsRouter)
