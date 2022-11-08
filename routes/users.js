0d21b38d3543bf56dc8f3936285edc55214c2867
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-11-08 18:29:04 -0500

    feat: add middleware

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,18 @@ const router = express.Router()
 
 let users = []
 
+const middlewareCoder = (req, res, next) => {
+    let user = req.body
+    if (!user.name || !user.lastname || !user.age) return res.status(400).send({err:'Data is required!'})
+    next()
+}
+
 router.get('/', (req, res) => {
     res.send({users})
 })
 
-router.post('/', (req, res) => {
+router.post('/', middlewareCoder, (req, res) => {
     let user = req.body
-    if (!user.name || !user.lastname || !user.age) return res.status(400).send({err:'Data is required!'})
     users.push(user)
     res.send({message:'User created!', user, users})
 })
